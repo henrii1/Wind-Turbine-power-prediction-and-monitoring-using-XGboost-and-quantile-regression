d1878080e37d9ff5eadfedc954ed5ed2dea77692
Emerald <emerald.henry@stu.cu.edu.ng>
2022-08-09 07:52:29 +0100

    first update

---
+++ loggings.py
@@ -0,0 +1,10 @@
+import logging
+logger = logging.getLogger(__name__)
+logger = logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s:%(created)f:%(filename)s')
+file_handler = logging.FileHandler('test.log')
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
